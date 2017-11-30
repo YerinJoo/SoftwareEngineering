e948310e16d1af87d1c5fdbbfcdc9510fd67e656
DESKTOP-477E0J6\JooYerin <flamephlox@gmail.com>
2017-11-30 23:16:20 +0900

    added class Calcultor

--- src/SimpleProgram.java
+++ src/SimpleProgram.java
@@ -1,6 +1,66 @@
 import java.util.*;
 
 public class SimpleProgram {
+
+class Calculator {
+		StringTokenizer tokenizer;
+		String token;
+		
+		public Calculator(String line) {
+			tokenizer = new StringTokenizer(line);
+			token = tokenizer.nextToken();
+		}
+		public double Evaluate() {  
+	        return Expression();  
+	    }  
+	     
+	    private double Primary() {  
+	        double result;  
+	        if(token.equals("")) {  
+	            token = tokenizer.nextToken();  
+	            result = Expression();  
+	        }  
+	        else result = Double.valueOf(token).doubleValue();  
+	       
+	        token = tokenizer.nextToken();  
+	        return result;  
+	    }  
+	     
+	    private double Term() {  
+	        double nextValue;  
+	        double result;
+	        result = Primary();  
+	        while(token.equals("*")) {  
+	            token = tokenizer.nextToken();  
+	            nextValue = Primary();  
+	            result *= nextValue;  
+	        }  
+	        while(token.equals("/")) {  
+	            token = tokenizer.nextToken();  
+	            nextValue = Primary();  
+	            result /= nextValue;  
+	        }    
+	        return result; 
+	    }  
+	     
+	    private double Expression() {  
+	        double nextValue;  
+	        double result;  
+	        result = Term();  
+	        while(token.equals("+")) {            
+	            token = tokenizer.nextToken();  
+	            nextValue = Term();  
+	            result += nextValue;  
+	        }  
+	        while(token.equals("-")) {  
+	            token = tokenizer.nextToken();  
+	            nextValue = Term();  
+	            result -= nextValue;  
+	        }  
+	        return result;  
+	     }  
+	}//class Calculator end
+
 	public static void main(String args[]){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("심플 프로그램입니다. 1.메모장 2.계산기 3.가계부 4.종료");
