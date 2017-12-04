9cec5df435f0430e645c0b52f21383d4c4b23e66
SohhyangLee <sohhyang7@gmail.com>
2017-12-05 01:17:17 +0900

    새로 만든 파일이얌

---
+++ eclipse-workspace/SimpleSoftware/src/accountbook_main.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+import java.util.*;
+
+public class accountbook_main {
+
+	public static void main(String[] args) {
+		AccountBook2 account = new AccountBook2();
+		Scanner scan = new Scanner(System.in);
+		int num = 0;
+		while(num!=4) {
+		System.out.print("[MENU]\n3.가계부\n4.종료\n->");
+		num = scan.nextInt();
+		switch(num) {
+		case 3 :
+			account.AccountMenu();
+			break;
+		case 4 :
+			System.out.print("시스템을 종료합니다.");
+			break;
+		}
+		}
+	}
+
+}
