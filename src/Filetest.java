8345358a680161066138b25fe51e1b25588c5a53
DESKTOP-477E0J6\JooYerin <flamephlox@gmail.com>
2017-12-23 22:57:41 +0900

    add Filetest.java

---
+++ src/Filetest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class Filetest {
+
+SimpleProgram sp = new SimpleProgram();
+	
+	AccountBook2 account = new AccountBook2();
+	String title = "test.txt";
+	String title2 = "test2.txt";
+	
+	@Test
+	public void testCreate() throws IOException{
+		account.CreateAccountBookText(title);
+		assertTrue(account.isExistbook(title));
+	}
+	
+	@Test
+	public void testDelete() throws IOException{
+		account.DeleteAccountBook(title);
+		assertTrue(account.isExistbook(title));
+
+	}
+
+}
