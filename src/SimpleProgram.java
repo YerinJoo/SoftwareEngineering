ec27c9e997e2078a59a0f46173d091999c7d1f8c
YerinJoo <33588880+YerinJoo@users.noreply.github.com>
2017-12-23 23:28:49 +0900

    Update SimpleProgram.java
    modify class Memo

--- src/SimpleProgram.java
+++ src/SimpleProgram.java
@@ -5,183 +5,187 @@ import java.util.*;
 
 public class SimpleProgram {
 	static class Memo {
-		void createMemo(String title) throws IOException{
-			Scanner sc = new Scanner(System.in);
-			String source = "";
-			String realtitle=title+".txt";
-			
-			System.out.print("메모 내용을 입력ㄱ : ");
-			source=sc.nextLine();
-			
-			char input[] = new char[source.length()];
-			source.getChars(0, source.length(), input, 0);
-			FileWriter fw = new FileWriter(realtitle);
-			fw.write(input);
-			fw.close();
-			System.out.print(title + " 파일이 생성되었습니다\n\n");
+         void createMemo(String title) throws IOException{
+            Scanner sc = new Scanner(System.in);
+            String source = "";
+            String realtitle=title+".txt";
+            
+            
+            System.out.print("메모 내용을 입력ㄱ : ");
+            
+            source=sc.nextLine();
+            
+            char input[] = new char[source.length()];
+            source.getChars(0, source.length(), input, 0);
+            FileWriter fw = new FileWriter(realtitle);
+            fw.write(input);
+            fw.close();
+            System.out.print(title + " 파일이 생성되었습니다\n\n");
 
-		}
-		
-		Boolean isExistMemo(String title){
-			File path = new File(".");
-			final String ext = ".txt";
-			
-			String fileList[] = path.list(new FilenameFilter(){
-				public boolean accept(File dir, String name){
-					
-					return name.endsWith(ext);
-				}
-			});
-			
-			File file = new File(path+"\\"+title+".txt");
+         }
+         
+         Boolean isExistMemo(String title){
+            
+      //      String pathS = SimpleProgram.class.getResource("").getPath();
+            File path = new File(".");
+            final String ext = ".txt";
+            
+            String fileList[] = path.list(new FilenameFilter(){
+               public boolean accept(File dir, String name){
+                  
+                  return name.endsWith(ext);
+               }
+            });
+            
+            File file = new File(path+"\\"+title+".txt");
 
-			if(file.exists())
-				return true;
-			else
-				return false;
-		}
-		
-		void deleteMemo(String title){
-		    Scanner scan = new Scanner(System.in);
-		    String realtitle=title+".txt";
-			
-		    File f = new File(realtitle);
-	
-		    if (f.delete()) {
-		      System.out.println("파일 또는 디렉토리를 성공적으로 지웠습니다: " + title + "\n\n");
-			
+            if(file.exists())
+               return true;
+            else
+               return false;
+         }
+         
+         void deleteMemo(String title){
+             Scanner scan = new Scanner(System.in);
+             String realtitle=title+".txt";
+            
+             File f = new File(realtitle);
+      
+             if (f.delete()) {
+               System.out.println("파일 또는 디렉토리를 성공적으로 지웠습니다: " + title + "\n\n");
+            
 
-		    } else {
-		      System.err.println("파일 또는 디렉토리 지우기 실패: " + title);
-		    }
-		}
-		
-		void updateMemo(String title) throws IOException{
-			Scanner sc = new Scanner(System.in);
-			String realtitle=title+".txt";
-			String source = "";
-			
-			File path = new File("C:\\Users\\ggg\\workspace\\swe1");
-			final String ext = ".txt";
-			
-			String fileList[] = path.list(new FilenameFilter(){
-				public boolean accept(File dir, String name){
-					
-					return name.endsWith(ext);
-				}
-			});
-			
-			
-			if(fileList.length > 0){				
-				for(int j=0; j < fileList.length; j++){
-					if(realtitle.equals(fileList[j])){
-						System.out.print("수정할 메모 내용을 입력ㄱ : ");
-						source=sc.nextLine();
-						char input[] = new char[source.length()];
-						source.getChars(0, source.length(), input, 0);
-						FileWriter fw = new FileWriter(realtitle);
-						fw.write(input);
-						fw.close();
-						System.out.print(title + "파일이 수정되었습니다\n\n");
-						break;
-					}
-					else{
-						System.out.println("일치하는 파일이 없습니다.");
-						break;
-					}
-						
-				}
-			}
-		}
-		
-		void openMemo() throws IOException{
-			Scanner sc = new Scanner(System.in);
-			String title="";
-			
-			int i;
-			
-			File path = new File("C:\\Users\\ggg\\workspace\\swe1");
-			final String ext = ".txt";
-			
-			String fileList[] = path.list(new FilenameFilter(){
-				public boolean accept(File dir, String name){
-					
-					return name.endsWith(ext);
-				}
-			});
-			
-			System.out.println("< 현재 파일 목록 >");
-			if(fileList.length > 0){				
-				for(int j=0; j < fileList.length; j++){
-					System.out.println(fileList[j]);
-				}
-			}
-			
+             } else {
+               System.err.println("파일 또는 디렉토리 지우기 실패: " + title);
+             }
+         }
+         
+         void updateMemo(String title) throws IOException{
+            Scanner sc = new Scanner(System.in);
+            String realtitle=title+".txt";
+            String source = "";
+            int a=0;
+            
+         //   String pathS = SimpleProgram.class.getResource("").getPath();
+            File path = new File(".");
+            final String ext = ".txt";
+            
+            String fileList[] = path.list(new FilenameFilter(){
+               public boolean accept(File dir, String name){
+                  
+                  return name.endsWith(ext);
+               }
+            });
+            
+            
+            if(fileList.length > 0){            
+               for(int j=0; j < fileList.length; j++){
+                  if(realtitle.equals(fileList[j])){
+                     System.out.print("수정할 메모 내용을 입력ㄱ : ");
+                     source=sc.nextLine();
+                     char input[] = new char[source.length()];
+                     source.getChars(0, source.length(), input, 0);
+                     FileWriter fw = new FileWriter(realtitle);
+                     fw.write(input);
+                     fw.close();
+                     System.out.print(title + "파일이 수정되었습니다\n\n");
+                     a=1;
+                     break;
+                  }                  
+               }
+            }
+            if(a==0){
+               System.out.println("일치하는 파일이 없습니다.");
+            }
+            
+            
+         }
+         
+         void openMemo() throws IOException{
+            Scanner sc = new Scanner(System.in);
+            String title="";
+            
+            int i;
+            
+         //   String pathS = SimpleProgram.class.getResource("").getPath();
+            File path = new File(".");
+            final String ext = ".txt";
+            
+            String fileList[] = path.list(new FilenameFilter(){
+               public boolean accept(File dir, String name){
+                  
+                  return name.endsWith(ext);
+               }
+            });
+            
+            System.out.println("< 현재 파일 목록 >");
+            if(fileList.length > 0){            
+               for(int j=0; j < fileList.length; j++){
+                  System.out.println(fileList[j]);
+               }
+            }
 
-			System.out.print("열고 싶은 메모 파일명: ");
-		    title = sc.next();
-		    
-		    String realtitle=title+".txt";
-			String source = "";
-			FileReader fr = new FileReader(realtitle);
-			
-			System.out.print("\n파일명 : " + realtitle + "\n메모 내용 : ");
-			
-			while((i=fr.read()) != -1)
-				System.out.print((char)i);
-			
-			System.out.println("\n");
-			
-			fr.close();	
-		}	
-	}
-	
-		void memomenu() throws IOException{
-			Scanner sc = new Scanner(System.in);
-		
-			int sel=0;
-			String title="";
-			Memo ddd = new Memo();
-			
-			do{
-				System.out.println("\n한 줄의 메모를 저장할 수 있는 메모장입니다. 메모 파일은 txt 파일로 저장됩니다. ");
-				System.out.print("1.메모추가\n2.메모삭제\n3.메모수정\n4.메모보기\n5.메인 메뉴로 돌아가기\n");
-				sel = sc.nextInt();
-	
-				switch(sel){
-				case 1:
-					System.out.print("추가할 파일명: ");
-					title = sc.next();
-					ddd.createMemo(title);
-					break;
-					
-				case 2:
-					System.out.print("지울 파일명: ");
-				    title = sc.next();
-					ddd.deleteMemo(title);
-					break;
-					
-				case 3:
-					System.out.print("수정할 파일명: ");
-				    title = sc.next();
-					ddd.updateMemo(title);
-					break;
-					
-				case 4:	
-					
-					ddd.openMemo();
-					break;
-					
-				case 5:
-					
-				//	System.exit(0);
-					break;
-				}
-			}while(sel != 5);
-			
-		} // class Memo end
-		
-		
+            System.out.print("열고 싶은 메모 파일명: ");
+             title = sc.next();
+             
+             String realtitle=title+".txt";
+            String source = "";
+            FileReader fr = new FileReader(realtitle);
+            
+            System.out.print("\n파일명 : " + realtitle + "\n메모 내용 : ");
+            
+            while((i=fr.read()) != -1)
+               System.out.print((char)i);
+            
+            System.out.println("\n");
+            fr.close();
+         }  
+      }
+      
+         void memomenu() throws IOException{
+            Scanner sc = new Scanner(System.in);
+            int sel=0;
+            String title="";
+            Memo ddd = new Memo();
+            
+            do{
+               System.out.println("\n한 줄의 메모를 저장할 수 있는 메모장입니다. 메모 파일은 txt 파일로 저장됩니다. ");
+               System.out.print("1.메모추가\n2.메모삭제\n3.메모수정\n4.메모보기\n5.메인 메뉴로 돌아가기\n");
+               sel = sc.nextInt();
+      
+               switch(sel){
+               case 1:
+                  System.out.print("추가할 파일명: ");
+                  title = sc.next();
+                  ddd.createMemo(title);
+                  break;
+                  
+               case 2:
+                  System.out.print("지울 파일명: ");
+                   title = sc.next();
+                  ddd.deleteMemo(title);
+                  break;
+                  
+               case 3:
+                  System.out.print("수정할 파일명: ");
+                   title = sc.next();
+                  ddd.updateMemo(title);
+                  break;
+                  
+               case 4:   
+                  
+                  ddd.openMemo();
+                  break;
+                  
+               case 5:
+                  
+               //   System.exit(0);
+                  break;
+               }
+            }while(sel != 5);
+            
+         } // class Memo end
+
 		static class Calculator {
 			Scanner scan = new Scanner(System.in);
 			void menu() {
